bfe8812816ad9dc393dfc2d2877b5a2057b4e3f9
c0reac0de <candice.correa@gmail.com>
2018-05-29 14:10:10 -0400

    upload logic update index

---
+++ js/logic.js
@@ -0,0 +1,18 @@
+//get only two players by setting inital values and limits
+
+//start game once two players have joined
+
+//user 1 and 2 input
+    //store input
+function rpsGame(player1Choice){
+var p1Choice= player1Choice.id;
+console.log(p1Choice);
+}
+
+    //send to firebase
+
+//compare input to determine winner
+    //use data if/else if (or switch case)
+
+//output win/lose on scoreboard
+    //connect firebase retrieved data to specified element
